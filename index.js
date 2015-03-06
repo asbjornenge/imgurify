94905626fb1b34dfae58a1fcc997fbce0904a0ae
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-06 22:19:16 +0100

    Moving index to function - bugs on multiple files fix

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var through = require('through2');
-var i       = -1
-var head    = function(type) { i++; return i == 0 ? 'module.exports = "data:image/'+type+';base64,' : '' }
-var tail    = function()     { return '"' }
-var isImg   = function(file) {
+var head    = function(i,type) { return i == 0 ? 'module.exports = "data:image/'+type+';base64,' : '' }
+var tail    = function()       { return '"' }
+var isImg   = function(file)   {
     return (/\.((lit)?gif|png|jpg|jpeg)$/).exec(file);
 }
 
 module.exports = function (file) {
     if (!isImg(file)) return through()
-    var type = isImg(file)[1]
+    var type = isImg(file)[1], i = -1
     return through(
-        function (buf, enc, next) { this.push(head(type)); this.push(buf.toString('base64')); next() },
+        function (buf, enc, next) { i++; this.push(head(i,type)); this.push(buf.toString('base64')); next() },
         function (end) { this.push(tail()); end() }
     )
 }
