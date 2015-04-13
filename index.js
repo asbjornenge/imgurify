e33fa3703dc03ce88acd12664a388e110a3060b3
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-04-13 09:52:49 +0200

    Removed svgo - should be a manual step since it might f*** the svg

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var through    = require('through2')
-var fs         = require('fs')
-var svgo_      = require('svgo')
-var svgo       = new svgo_()
+var esc        = require('js-string-escape')
 var rasterHead = function(i,type) { return i == 0 ? 'module.exports = "data:image/'+type+';base64,' : '' }
-var svgHead    = function()       { return "module.exports = 'data:image/svg+xml;utf8," }
+var svgHead    = function(i,type) { return i == 0 ? "module.exports = 'data:image/svg+xml;utf8," : '' }
 var rasterTail = function()       { return '"' }
 var svgTail    = function()       { return "'" }
 var isImg      = function(file)   {
@@ -18,21 +16,17 @@ function rasterStream(file, type) {
     )
 }
 
-// TODO: I know this is really silly, reading it twice,
-// but I couldn't for the life of me figure out how to read and replace all the data
-// on the end function ... please make ISSUE or PR on github if you know!
-function svgStream(file) {
+function svgStream(file, type) {
     var i = -1
-    var raw = fs.readFileSync(file,'utf-8')
     return through(
-        function (buf, enc, next) { next() },
-        function (end) { svgo.optimize(raw, function(res) { this.push(svgHead()); this.push(res.data); this.push(svgTail()); end() }.bind(this)) }
+        function (buf, enc, next) { i++; this.push(svgHead(i,type)); this.push(esc(buf.toString('utf-8'))); next() },
+        function (end) { this.push(svgTail()); end() }
     )
 }
 
 module.exports = function (file) {
     if (!isImg(file)) return through()
     var type = isImg(file)[1]
-    if (type == 'svg') return svgStream(file)
+    if (type == 'svg') return svgStream(file, type)
     else return rasterStream(file, type)
 }
