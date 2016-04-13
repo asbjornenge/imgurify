d192b2988f1ead0838c438329f6e191625424787
Asbjørn Enge <asbjorn@hanafjedle.net>
2016-04-13 23:47:51 +0200

    Rewrote the whole thing. Simplified a lot. Using mime. Reading whole file sync with correct encoding. Should fix both #3 and #4

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
-var through    = require('through2')
-var rasterHead = function(i,type) { return i == 0 ? "module.exports = 'data:image/"+getMimeType(type)+";base64," : '' }
-var svgHead    = function(i,type) { return i == 0 ? "module.exports = 'data:image/svg+xml;charset=utf8," : '' }
-var rasterTail = function()       { return "'" }
-var svgTail    = function()       { return "'" }
-var isImg      = function(file)   {
-    return (/\.((lit)?gif|png|jpg|jpeg|svg)$/).exec(file);
-}
-var getMimeType   = function(type) {
-    if (type == 'jpg') return 'jpeg'
-    return type
+var fs = require('fs')
+var mime = require('mime')
+var through = require('through2')
+var isImg = function(file) { return (/\.((lit)?gif|png|jpg|jpeg|svg)$/).exec(file) }
+
+module.exports = function(file) {
+  if (!isImg(file)) return through()
+  var type = mime.lookup(file)
+  var enc  = type == 'image/svg+xml' ? 'utf-8' : 'base64'
+  var meta = type == 'image/svg+xml' ? ';charset=utf8,' : ';base64,'
+  var data = fs.readFileSync(file).toString(enc)
+  return through(
+    function(buf, enc, cb) { cb() },
+    function(end) {
+      var mod = "module.exports = 'data:"+type+meta+data+"'"
+      this.push(mod)
+      end()
+    }
+  )
 }
 
-function rasterStream(file, type) {
-    var i = -1
-    var buffers = [] 
-    return through(
-        function (buf, enc, next) { 
-          i++; 
-          if (i == 0) { this.push(rasterHead(i,type)) } 
-          buffers.push(buf)
-          next() 
-        },
-        function (end) { 
-          var totalLength = buffers.reduce(function(length, buf) {
-            return length + buf.length
-          },0)
-          var finalBuffer = Buffer.concat(buffers, totalLength)
-          this.push(finalBuffer.toString('base64'))
-          this.push(rasterTail())
-          end() 
-        }
-    )
-}
-
-function svgStream(file, type) {
-    var i = -1
-    return through(
-        function (buf, enc, next) { 
-          i++; 
-          this.push(svgHead(i,type))
-          this.push(buf.toString('utf-8'))
-          next() 
-        },
-        function (end) { 
-          this.push(svgTail())
-          end() 
-        }
-    )
-}
-
-module.exports = function (file) {
-    if (!isImg(file)) return through()
-    var type = isImg(file)[1]
-    if (type == 'svg') return svgStream(file, type)
-    else return rasterStream(file, type)
-}
