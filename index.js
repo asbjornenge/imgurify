e2191e4a46f66eacc03ad5d21de0d9092bbfad5f
Pete Feltham <dev@felthy.com>
2018-01-09 11:28:34 +1100

    Update svg encoding with latest recommendations from https://css-tricks.com/probably-dont-base64-svg/; fix newlines breaking encoding

--- index.js
+++ index.js
@@ -3,16 +3,31 @@ var mime = require('mime')
 var through = require('through2')
 var isImg = function(file) { return (/\.((lit)?gif|png|jpg|jpeg|svg)$/).exec(file) }
 
+function tidySvg(svg) {
+  return encodeURIComponent(
+      svg.replace(/\n+/g, ' ').trim()  // remove newlines
+    ) // encode URL-unsafe characters
+    .replace(/%20/g, ' ') // put spaces back in
+    .replace(/%3D/g, '=') // ditto equals signs
+    .replace(/%3A/g, ':') // ditto colons
+    .replace(/%2F/g, '/') // ditto slashes
+    .replace(/%22/g, "'") // replace quotes with apostrophes (may break certain SVGs)
+}
+
 module.exports = function(file) {
   if (!isImg(file)) return through()
   var type = mime.lookup(file)
-  var enc  = type == 'image/svg+xml' ? 'utf-8' : 'base64'
-  var meta = type == 'image/svg+xml' ? ';charset=utf8,' : ';base64,'
+  var isSVG = type == 'image/svg+xml'
+  var enc  = isSVG ? 'utf-8' : 'base64'
+  var meta = isSVG ? ',' : ';base64,'
   var data = fs.readFileSync(file).toString(enc)
+  if (isSVG) {
+    data = tidySvg(data)
+  }
   return through(
     function(buf, enc, cb) { cb() },
     function(end) {
-      var mod = "module.exports = 'data:"+type+meta+data+"'"
+      var mod = 'module.exports = "data:'+type+meta+data+'"'
       this.push(mod)
       end()
     }
--- testapp/app.js
+++ testapp/app.js
@@ -1,24 +1,32 @@
-var red     = require('../test/red.png')
-var pony    = require('../test/pony.jpg')
-var logo    = require('../test/javascript-logo.png')
-var thumb   = require('../test/thumb.svg')
-var spinner = require('../test/spinner.gif')
+var red          = require('../test/red.png')
+var pony         = require('../test/pony.jpg')
+var logo         = require('../test/javascript-logo.png')
+var thumb        = require('../test/thumb.svg')
+var newlines     = require('../test/newlines.svg')
+var singlequotes = require('../test/singlequotes.svg')
+var spinner      = require('../test/spinner.gif')
 
 var container = document.createElement('div')
 var _pony = document.createElement('img')
 var _logo = document.createElement('img')
 var _thumb = document.createElement('img')
+var _newlines = document.createElement('img')
+var _singlequotes = document.createElement('img')
 var _spinner = document.createElement('img')
 var _red = document.createElement('img')
 _pony.src = pony
 _logo.src = logo
 _thumb.src = thumb
+_newlines.src = newlines
+_singlequotes.src = singlequotes
 _spinner.src = spinner
 _red.src = red
 container.appendChild(_pony)
 container.appendChild(_logo)
 container.appendChild(_spinner)
 container.appendChild(_thumb)
+container.appendChild(_newlines)
+container.appendChild(_singlequotes)
 container.appendChild(_red)
 document.body.appendChild(container)
 
